e3e6f4c63af9de0918bbdab4a100f031bf9898b5
juan pucheta <113641092+juangithubpro@users.noreply.github.com>
2024-10-21 22:20:37 -0300

    Update wsgi.py

--- blog/core/wsgi.py
+++ blog/core/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for blog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, see.
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
